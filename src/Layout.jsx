aa1f96c6c3d1863310a39245e3165e13b5fb6f0b
nav2924 <navi29012004@gmail.com>
2025-02-21 09:17:17 +0530

    init

---
+++ src/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]">Layout</div>
+      
+    </div>
+  )
+}
+
+export default Layout
