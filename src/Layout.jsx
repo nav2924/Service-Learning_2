482b8bf09227d3c74ecb0344ca36f6499a66a638
nav2924 <navi29012004@gmail.com>
2025-02-21 09:35:54 +0530

    navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Navbar } from "./components/index";
 
 const Layout = () => {
   return (
-    <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]">Layout</div></div>
-  )
-}
+    <div className="absolute inset-0 -z-10 h-full w-full bg-gradient-to-b from-slate-50 via-blue-100 to-blue-300">
+      <Navbar />
+    </div>
+  );
+};
 
 export default Layout
