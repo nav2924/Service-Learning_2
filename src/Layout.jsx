a892a85d144d6d715c49a60939673d29a02b310a
nav2924 <navi29012004@gmail.com>
2025-02-21 09:17:55 +0530

    init v2.0

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 const Layout = () => {
   return (
-    <div className="relative min-h-screen">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]">Layout</div>
-      
-    </div>
+    <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]">Layout</div></div>
   )
 }
 
